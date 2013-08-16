08773f83f42e1352ba8fc31060f61429a626ef9a
Nathan Bergey <nathan.bergey@gmail.com>
2013-08-16 01:31:01 -0700

    super basic GET of public calendar XML

--- server.py
+++ server.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template
+import httplib
+import xml.etree.ElementTree as ET
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/cal")
+def cal():
+    c = httplib.HTTPSConnection("www.google.com")
+    c.request("GET", "/calendar/feeds/epl.pdx%40gmail.com/public/basic")
+    response = c.getresponse()
+
+    root = ""
+    if response.status == 200:
+        data = response.read()
+        root = ET.fromstring(data)
+
+    for child in root:
+        print child.tag, child.attrib
+
+    return root.tag
 
 if __name__ == "__main__":
     app.debug = True
